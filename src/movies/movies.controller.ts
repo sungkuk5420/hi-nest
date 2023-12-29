00612e6dd0dbe1066a519a02af6fda63470ee401
kim sungkuk <sungkuk5420@gmail.com>
2023-12-29 11:33:46 +0900

    movie 컨트롤러와 데코레이터들 실습

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    Patch,
+    Body,
+    Query,
+  } from '@nestjs/common';
+  
+  @Controller('movies')
+  export class MoviesController {
+    @Get()
+    getAll() {
+      return 'This will return all movies';
+    }
+    @Get('search')
+    search(@Query('year') seachingYear: string) {
+      return `We are searching for a movie made after: ${seachingYear}`;
+    }
+  
+    @Get(':id')
+    getOne(@Param('id') movieId: string) {
+      return `This will return one movie with the id: ${movieId}`;
+    }
+  
+    @Post()
+    create(@Body() movieData) {
+      return movieData;
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') movieId: string) {
+      return `This will delete a movie with the id: ${movieId}`;
+    }
+  
+    @Patch(':id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+      return {
+        updatedMovie: movieId,
+        ...updateData,
+      };
+    }
+  }
