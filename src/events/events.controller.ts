31b980eddc35ffc4372da9238d5ccb1ba99eacbd
kim sungkuk <sungkuk5420@gmail.com>
2024-01-02 17:12:11 +0900

    이벤트 페이지 데이터 추가

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 
 import { EventsService } from './events.service';
 
@@ -7,8 +7,22 @@ export class EventsController {
     constructor(
         private readonly eventsService: EventsService,  // 의존성 주입
     ) {}
-    
-    
+    @Get()
+    getEventData(){
+        return {
+            title:'<div style="color:${mainTextColor};">ZEISS 스마일®<br>✨ <span style="color:${accentTextColor};">${variable}</span>번째 ✨<br>환자입니다!</div>',
+            mainTextColor: "#2F3337",
+            accentTextColor: "#2E6FEF",
+            variable: "1,200,252",
+            firstBackgroundColor: "white",
+            secondBackgroundColor: "black",
+            buttonColor: "#2E6FEF",
+            buttonTextColor: "white",
+            buttonText:"ZEISS 카드 받고 이벤트 참여",
+            imageURL:"https://previews.123rf.com/images/kchung/kchung1610/kchung161001354/64508202-test-written-by-hand-hand-writing-on-transparent-board-photo.jpg",
+        };
+    }
+    @Get('callback')
     incrementEventIndex(@Body() shareData){
         console.log("====shareData====",shareData)
         return this.eventsService.incrementEventIndex;
